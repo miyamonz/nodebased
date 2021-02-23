19c218501b2d74b3e976a5939429ca4ddc015824
Koichi Miyamoto <m5813k@gmail.com>
2021-02-23 17:02:12 +0900

    fix RenderNode

--- src/Node/NodeSwitcher.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import type { NodeComponent } from "./types";
-
-const NodeSwitcher: NodeComponent = ({ node }) => {
-  return <>{node.component !== undefined && <node.component node={node} />}</>;
-};
-
-export default React.memo(NodeSwitcher);
--- src/Node/RenderNode.tsx
+++ src/Node/RenderNode.tsx
@@ -1,31 +1,33 @@
 import React from "react";
 import { useAtom } from "jotai";
 import { hoveredNodeAtom } from "./atoms";
-import type { NodeAtom } from "./types";
-import NodeSwitcher from "./NodeSwitcher";
+import type { NodeAtom, NodeComponent } from "./types";
 import { selectedRectAtomListAtom } from "../Select/drag";
 import { InputCircle, OutputCircle } from "../Socket";
 
-const RenderNode: React.FC<{ nodeAtom: NodeAtom }> = ({ nodeAtom }) => {
+type NodeAtomComponent = React.FC<{ nodeAtom: NodeAtom }>;
+
+const ShowSelect: NodeAtomComponent = ({ nodeAtom }) => {
   const [node] = useAtom(nodeAtom);
   const [rect] = useAtom(node.rect);
-
-  const [hovered, setHovered] = useAtom(hoveredNodeAtom);
-  const isHovered = nodeAtom === hovered;
-
   const [selectedNodes] = useAtom(selectedRectAtomListAtom);
   const isSelected = selectedNodes.includes(nodeAtom);
 
-  const center = { x: rect.x + rect.width / 2, y: rect.y + rect.height / 2 };
-  const [outValue] = useAtom(node.output.atom);
+  if (isSelected) {
+    return <rect {...rect} fill="lightpink" stroke="red" />;
+  }
+  return null;
+};
+
+const ShowHovered: NodeAtomComponent = ({ nodeAtom }) => {
+  const [node] = useAtom(nodeAtom);
+  const [rect] = useAtom(node.rect);
+  const [hovered, setHovered] = useAtom(hoveredNodeAtom);
+  const isHovered = nodeAtom === hovered;
+
   return (
     <>
-      {isSelected && <rect {...rect} fill="lightpink" stroke="red" />}
       {isHovered && <rect {...rect} fill="none" stroke="red" />}
-      <text {...center}>
-        {(typeof outValue === "number" || typeof outValue === "string") &&
-          outValue}
-      </text>
       <rect
         {...rect}
         fill="transparent"
@@ -34,11 +36,28 @@ const RenderNode: React.FC<{ nodeAtom: NodeAtom }> = ({ nodeAtom }) => {
         onMouseMove={() => setHovered(nodeAtom)}
         onMouseLeave={() => setHovered(null)}
       />
+    </>
+  );
+};
+
+const RenderNode: React.FC<{ nodeAtom: NodeAtom }> = ({ nodeAtom }) => {
+  const [node] = useAtom(nodeAtom);
+  const [rect] = useAtom(node.rect);
+
+  const center = { x: rect.x + rect.width / 2, y: rect.y + rect.height / 2 };
+  const [outValue] = useAtom(node.output.atom);
+  return (
+    <>
+      <ShowSelect nodeAtom={nodeAtom} />
+      <text {...center}>
+        {(typeof outValue === "number" || typeof outValue === "string") &&
+          outValue}
+      </text>
+      <ShowHovered nodeAtom={nodeAtom} />
       <g transform={`translate(${rect.x} ${rect.y - 5} )`}>
         <text>{node.name}</text>
       </g>
-      <NodeSwitcher node={node} />
-
+      <>{node.component !== undefined && <node.component node={node} />}</>
       {node.inputs.map((input) => {
         return <InputCircle key={input.atom.toString()} input={input} />;
       })}
