077461dc0ed3226a3ac0e0ca1e3d1a8014aec4ba
Koichi Miyamoto <m5813k@gmail.com>
2021-02-26 21:26:23 +0900

    fix to use useMouseStream

--- src/MoveNode/drag.ts
+++ src/MoveNode/drag.ts
@@ -1,8 +1,10 @@
-import { atom } from "jotai";
+import { useEffect } from "react";
+import { atom, useAtom } from "jotai";
 import { hoveredNodeAtom } from "../Node";
 import { selectedRectAtomListAtom } from "../Select/drag";
 import type { Position } from "../Position";
 import { useEvent, Event } from "../SVGContext";
+import { useMouseStream } from "../SVGContext";
 
 const isSetByClick = atom(false);
 
@@ -21,52 +23,52 @@ const dragAtomToMoveNode = atom(null, (get, set, e: Event) => {
       set(isSetByClick, false);
     }
   }
+});
 
-  set(dragAtomToMoveNode_, e);
+const positionsWhenGrabbed = atom<Map<string, Position>>(new Map());
+
+const setGrabAtom = atom(null, (get, set) => {
+  const dragTarget = get(selectedRectAtomListAtom);
+  if (dragTarget === null) return;
+  const keyValues = dragTarget.map((nodeAtom) => {
+    const key = nodeAtom.toString();
+    const rect = get(get(nodeAtom).rect);
+    const pos = { x: rect.x, y: rect.y };
+    return [key, pos] as const;
+  });
+  set(positionsWhenGrabbed, new Map(keyValues));
+});
+
+const setDragDiffAtom = atom(null, (get, set, mouseDiff: Position) => {
+  const dragTarget = get(selectedRectAtomListAtom);
+  if (dragTarget === null) return;
+
+  const positions = get(positionsWhenGrabbed);
+
+  dragTarget.forEach((nodeAtom) => {
+    const startRectPos = positions.get(nodeAtom.toString());
+    if (startRectPos === undefined) throw new Error();
+    const moved = {
+      x: startRectPos.x + mouseDiff.x,
+      y: startRectPos.y + mouseDiff.y,
+    };
+    set(get(nodeAtom).rect, (prev) => ({ ...prev, ...moved }));
+  });
 });
 
 export function useDragMoveNode() {
-  return useEvent(dragAtomToMoveNode);
+  const { start, drag } = useMouseStream();
+
+  const [, setGrab] = useAtom(setGrabAtom);
+  useEffect(() => {
+    if (start === null) return;
+    setGrab();
+  }, [start]);
+  const [, setDragDiff] = useAtom(setDragDiffAtom);
+
+  useEffect(() => {
+    if (start === null || drag === null) return;
+    const mouseDiff = { x: drag.x - start.x, y: drag.y - start.y };
+    setDragDiff(mouseDiff);
+  }, [drag]);
 }
-
-const isDown = atom(false);
-const dragStartAtom = atom<Position | null>(null);
-const positionsWhenGrabbed = atom<Map<string, Position>>(new Map());
-export const dragAtomToMoveNode_ = atom(null, (get, set, e: Event) => {
-  const pos = e.position;
-  const dragTarget = get(selectedRectAtomListAtom);
-  if (e.type === "mousedown") {
-    set(isDown, true);
-    if (dragTarget) {
-      const keyValues = dragTarget.map((nodeAtom) => {
-        const key = nodeAtom.toString();
-        const rect = get(get(nodeAtom).rect);
-        const pos = { x: rect.x, y: rect.y };
-        return [key, pos] as const;
-      });
-      set(positionsWhenGrabbed, new Map(keyValues));
-    }
-
-    set(dragStartAtom, { x: pos.x, y: pos.y });
-  } else if (e.type === "mousemove" && get(isDown)) {
-    const startPos = get(dragStartAtom);
-    if (startPos === null) throw new Error();
-    const mouseDiff = { x: pos.x - startPos.x, y: pos.y - startPos.y };
-    if (dragTarget) {
-      const positions = get(positionsWhenGrabbed);
-
-      dragTarget.forEach((nodeAtom) => {
-        const startRectPos = positions.get(nodeAtom.toString());
-        if (startRectPos === undefined) throw new Error();
-        const moved = {
-          x: startRectPos.x + mouseDiff.x,
-          y: startRectPos.y + mouseDiff.y,
-        };
-        set(get(nodeAtom).rect, (prev) => ({ ...prev, ...moved }));
-      });
-    }
-  } else if (e.type === "mouseup") {
-    set(isDown, false);
-    set(dragStartAtom, null);
-  }
-});
