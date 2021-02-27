92ba2b5518c069af615b378c13acf2d326014b2a
Koichi Miyamoto <m5813k@gmail.com>
2021-02-28 02:43:24 +0900

    fix render node

--- src/NodeList/RenderElementNode.tsx
+++ src/NodeList/RenderElementNode.tsx
@@ -2,18 +2,17 @@ import React from "react";
 import { useAtom } from "jotai";
 import type { NodeComponent } from "../Node";
 
+const isValidElement = (input: unknown): input is React.ReactElement =>
+  typeof input === "object" && React.isValidElement(input);
+
 export const RenderElementNode: NodeComponent = ({ node }) => {
   const [inputs] = useAtom(node.inputValues);
 
   return (
     <>
-      {inputs
-        .filter(
-          (input) => typeof input === "object" && React.isValidElement(input)
-        )
-        .map((input) => (
-          <g key={input.toString()}>{input}</g>
-        ))}
+      {inputs.filter(isValidElement).map((input) => (
+        <g key={input.toString()}>{input}</g>
+      ))}
     </>
   );
 };
