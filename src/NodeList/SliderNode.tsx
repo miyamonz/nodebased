1c2a68dbbe4d97aabe81817d3d407d7859462d9d
Koichi Miyamoto <m5813k@gmail.com>
2021-02-27 16:16:34 +0900

    prevent set slider value when input is connected

--- src/NodeList/SliderNode.tsx
+++ src/NodeList/SliderNode.tsx
@@ -2,26 +2,26 @@ import React from "react";
 import { useAtom } from "jotai";
 import type { Atom } from "jotai";
 import type { NodeComponent } from "../Node";
-import type { InputSocketNotConnected } from "../Socket";
+import { InputSocket, isConnected } from "../Socket";
 
 export const SliderNode: NodeComponent = ({ node }) => {
-  const isocket = node.inputs[0] as InputSocketNotConnected<number>;
+  const isocket = node.inputs[0] as InputSocket<number>;
   const [inputAtom] = useAtom(isocket.atom);
   const [, setInput] = useAtom(inputAtom);
 
   const [num] = useAtom(node.output.atom as Atom<number>);
 
   const [rect] = useAtom(node.rect);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (isConnected(isocket)) return;
+    // @ts-ignore-next-line
+    setInput(parseInt(e.target.value, 10));
+  };
   return (
     <>
       <foreignObject {...rect} y={rect.y + rect.height}>
-        <input
-          type="range"
-          min={0}
-          max={100}
-          value={num}
-          onChange={(e) => setInput(parseInt(e.target.value, 10))}
-        />
+        <input type="range" min={0} max={100} value={num} onChange={onChange} />
       </foreignObject>
     </>
   );
--- src/Socket/index.ts
+++ src/Socket/index.ts
@@ -1,3 +1,4 @@
+export { isConnected } from "./types";
 export type {
   InputSocket,
   InputSocketConnected,
