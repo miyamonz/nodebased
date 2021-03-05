2d3d527407b26da6e23a71ada4f54bc58e4bf350
Koichi Miyamoto <m5813k@gmail.com>
2021-03-06 04:15:06 +0900

    fix button

---
+++ src/NodeList/button.tsx
@@ -0,0 +1,57 @@
+import { useMemo } from "react";
+import { atom, useAtom } from "jotai";
+import type { WritableAtom } from "jotai";
+import { createAtomRef } from "../AtomRef";
+import { createVariable } from "../Variable";
+import type { NodeComponent } from "../Node";
+
+function getComponent(stateAtom: WritableAtom<boolean, boolean>) {
+  const RenderButtonNode: NodeComponent = ({ node }) => {
+    const [rect] = useAtom(node.rect);
+    const center = { x: rect.x + rect.width / 2, y: rect.y + rect.height / 2 };
+    const [isDown, setDown] = useAtom(stateAtom);
+
+    const r = useMemo(() => (Math.min(rect.width, rect.height) / 2) * 0.8, [
+      rect.width,
+      rect.height,
+    ]);
+
+    return (
+      <>
+        <circle
+          cx={center.x}
+          cy={center.y}
+          onMouseDown={() => setDown(true)}
+          onMouseUp={() => setDown(false)}
+          r={r}
+          stroke="blue"
+          fill={isDown ? "blue" : "transparent"}
+        />
+      </>
+    );
+  };
+  return RenderButtonNode;
+}
+
+const option = {
+  name: "button",
+  init: () => {
+    const input = createAtomRef(atom(false));
+    const buttonAtom = atom(false);
+    const variable = createVariable([input], (input_) => {
+      return atom((get) => {
+        const [in_] = get(input_);
+        return Boolean(in_ || get(buttonAtom));
+      });
+    });
+    const stateAtom = atom(
+      (get) => get(variable.outputAtoms[0]),
+      (_get, set, arg: boolean) => set(buttonAtom, arg)
+    );
+    return {
+      component: getComponent(stateAtom),
+      variable,
+    };
+  },
+};
+export default option;
--- src/NodeList/components/index.ts
+++ src/NodeList/components/index.ts
@@ -1,4 +1,3 @@
 export { default as RenderComponentNode } from "./RenderComponentNode";
 export { default as RenderAtomNode } from "./RenderAtomNode";
 export { default as SliderNode } from "./SliderNode";
-export { default as RenderButtonNode } from "./RenderButtonNode";
