5a34aaefb407783e003bfd27f3dd43a86fd142cc
Koichi Miyamoto <m5813k@gmail.com>
2021-03-06 23:54:46 +0900

    fix render to input multiple components

--- src/NodeList/render.tsx
+++ src/NodeList/render.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import { atom, useAtom } from "jotai";
+import { useAtomValue } from "jotai/utils";
 import { createAtomRef } from "../AtomRef";
 
+const range = (n: number) => [...Array(n).keys()];
 const option = {
   name: "render",
   init: () => {
-    const componentAtomRef = createAtomRef(
-      atom<React.ComponentType | null>(null)
-    );
-    const inputAtoms = [componentAtomRef];
+    const inputAtoms = range(5).map(() => {
+      return createAtomRef(atom<React.ComponentType | null>(null));
+    });
+
+    const componentsAtom = atom((get) => {
+      return inputAtoms.map(get).map(get);
+    });
 
     const Render = () => {
-      const [componentAtom] = useAtom(componentAtomRef);
-      const [Component] = useAtom(componentAtom);
-      if (Component === null) return null;
-      return <Component />;
+      const components = useAtomValue(componentsAtom);
+      return (
+        <>
+          {components
+            .filter((c): c is React.ComponentType => c !== null)
+            .map((Component, i) => (
+              <Component key={i} />
+            ))}
+        </>
+      );
     };
     const outputAtoms = [atom(() => undefined)];
     const variable = { inputAtoms, outputAtoms };
