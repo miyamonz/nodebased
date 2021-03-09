eea7d29fd1a1a04644193f67421c2bad3e4270fa
Koichi Miyamoto <m5813k@gmail.com>
2021-03-10 02:49:27 +0900

    fix copy json

--- src/Select/atoms.ts
+++ src/Select/atoms.ts
@@ -1,29 +1,28 @@
 import { atom } from "jotai";
 import { useAtomValue, useUpdateAtom } from "jotai/utils";
-import { nodeToJson } from "../Node";
 import type { Node } from "../Node";
-import { connectionAtom, filterConnection, Connection } from "../Connect";
-import { connectionToJson } from "../Connect/json";
 import { copyToClipboard } from "../util";
+
+import { createGraph, graphToJson } from "../Graph";
 
 // selected nodes
 export const selectedNodesAtom = atom<Node[]>([]);
 export function useSelectedNodes() {
   return useAtomValue(selectedNodesAtom);
 }
-
 export function useSetSelected() {
   return useUpdateAtom(selectedNodesAtom);
 }
 
+const selectedGraphAtom = atom((get) => {
+  const selectedNodes = get(selectedNodesAtom);
+  return createGraph(selectedNodes);
+});
+
 const selectedAtomJSON = atom(
   (get) => {
-    const nodes = get(selectedNodesAtom);
-    const connections = get(selectedConnectionsAtom);
-    return {
-      nodes: nodes.map(nodeToJson(get)),
-      connections: connections.map(connectionToJson(nodes)),
-    };
+    const graph = get(selectedGraphAtom);
+    return graphToJson(get)(graph);
   },
   (get) => {
     const json = get(selectedAtomJSON);
@@ -35,11 +34,3 @@ export function useCopyToClipboard() {
   const set = useUpdateAtom(selectedAtomJSON);
   return () => set();
 }
-
-// connection
-export const selectedConnectionsAtom = atom<Connection<unknown>[]>((get) => {
-  const selectedNodes = get(selectedNodesAtom);
-  const connections = get(connectionAtom);
-
-  return filterConnection(selectedNodes, connections);
-});
