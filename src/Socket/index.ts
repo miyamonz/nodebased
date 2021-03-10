f3adc24f848ef02cb3088e088108b390356fec05
Koichi Miyamoto <m5813k@gmail.com>
2021-03-11 02:15:48 +0900

    remove export

--- src/Socket/index.ts
+++ src/Socket/index.ts
@@ -1,12 +1,7 @@
 export { useConnected } from "./utils";
 export type { InputSocket, OutputSocket } from "./types";
 
-export {
-  createInputSocket,
-  createInputSockets,
-  createOutputSocket,
-  createOutputSockets,
-} from "./func";
+export { createInputSockets, createOutputSockets } from "./func";
 export { InputCircle, OutputCircle } from "./IOCircle";
 export { hoveredInputSocketAtom, hoveredOutputSocketAtom } from "./IOCircle";
 
