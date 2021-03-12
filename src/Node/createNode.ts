359c388905ca4183b3a6742fb034abcd54c3ab42
Koichi Miyamoto <m5813k@gmail.com>
2021-03-12 23:33:15 +0900

    add createNode

--- src/Node/createNode.ts
+++ src/Node/createNode.ts
@@ -23,13 +23,30 @@ export function createNodeByName({ name, position, id, data }: Props): Node {
   if (option === undefined) throw new Error(`${name} not found`);
 
   const { variable, component, saveData } = option.init({ data });
+  return createNode({ name, position, id, variable, component, saveData });
+}
 
+type createNodeProp = {
+  name: string;
+  position: Position;
+  variable: Variable;
+  component: NodeComponent;
+  saveData: boolean;
+  id?: string;
+};
+export function createNode({
+  name,
+  position,
+  variable,
+  component,
+  saveData,
+  id,
+}: createNodeProp) {
   const rect = createRect(position);
   const inputSockets = createInputSockets(rect, variable.inputAtoms);
   const outputSockets = createOutputSockets(rect, variable.outputAtoms);
 
   id = id ?? Math.floor(Math.random() * 10 ** 12).toString();
-
   return {
     rect,
     inputs: inputSockets,
