9b78f521341d1726092f7e9deb426c845999f8a9
Koichi Miyamoto <m5813k@gmail.com>
2021-03-14 00:09:52 +0900

    fix hold behavior

--- src/NodeList/hold.ts
+++ src/NodeList/hold.ts
@@ -1,12 +1,12 @@
 import { atom } from "jotai";
 import { createAtomRef } from "../AtomRef";
+import type { Variable } from "../Variable";
 
 const option = {
   name: "hold",
   init: () => {
     const condAtom = createAtomRef(atom(false));
-    const inputAtom = createAtomRef(atom(null));
-    const inputAtoms = [condAtom, inputAtom];
+    const inputAtom = createAtomRef(atom<unknown>(null));
 
     const tmpAtom = atom(null);
     let setter: any;
@@ -21,13 +21,21 @@ const option = {
         const a = get(get(inputAtom));
         setter(a);
       } else {
+        const a = get(get(inputAtom));
+        if (
+          typeof a?.x === "number" &&
+          typeof a?.y === "number" &&
+          get(tmpAtom) === null &&
+          setter !== undefined
+        ) {
+          setter({ x: 0, y: 0 });
+        }
       }
       return get(tmpAtom);
     });
-    const outputAtoms = [outAtom];
-    const variable = {
-      inputAtoms,
-      outputAtoms,
+    const variable: Variable = {
+      inputAtoms: [condAtom, inputAtom as any],
+      outputAtoms: [outAtom],
     };
     return { variable };
   },
