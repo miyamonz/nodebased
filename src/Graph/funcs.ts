73ed8b16f8b94472afbf3cddcbf86d02043b1868
Koichi Miyamoto <m5813k@gmail.com>
2021-04-02 23:48:48 +0900

    fix

--- src/Graph/funcs.ts
+++ src/Graph/funcs.ts
@@ -5,11 +5,8 @@ import type { Graph, GraphView } from "./types";
 import type { Node } from "../Node";
 import type { Edge } from "../Edge";
 
-export function createGraph(
-  nodes: Node[],
-  edges: Edge<unknown>[] = []
-): Graph {
-  const nodesAtom = atom<Node[]>(nodes);
+export function createGraph(nodes: Node[], edges: Edge<unknown>[] = []): Graph {
+  const nodesAtom = atom(nodes);
   return {
     nodes: nodesAtom,
     edges: atom(edges),
